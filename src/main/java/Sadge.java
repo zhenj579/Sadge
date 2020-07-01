ed242d410e6b67edce8367ac41937c0274bccb57
zhenj579 <50351080+zhenj579@users.noreply.github.com>
2020-06-30 20:36:28 -0400

    modified the structure of the class
    added a listen function to listen to specific events

--- src/main/java/Sadge.java
+++ src/main/java/Sadge.java
@@ -1,28 +1,43 @@
-import discord4j.common.util.Snowflake;
+import Listeners.OnMessageCreateListener;
+import Utility.Utility;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.EventDispatcher;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.TextChannel;
 
 public class Sadge {
 
     private final DiscordClient client;
     private final GatewayDiscordClient gateway;
+    private final EventDispatcher eventDispatcher;
     private final Utility utility;
 
     public Sadge(String token)
     {
         client = DiscordClient.create(token);
         gateway = client.login().block();
+        eventDispatcher = gateway.getEventDispatcher();
         utility = new Utility(gateway);
-        start();
+    }
+
+    public void start()
+    {
+        listen();
         gateway.onDisconnect().block();
     }
 
-    private void start()
+    private void listen()
     {
-        TextChannel generalChannel = (TextChannel) utility.getGeneralChannel();
-        generalChannel.createMessage("Online!").block();
-
+        OnMessageCreateListener.run(eventDispatcher, (TextChannel)utility.getGeneralChannel());
+        eventDispatcher.on(MessageCreateEvent.class).subscribe(messageCreateEvent -> {
+            String messageContent = messageCreateEvent.getMessage().getContent();
+            if(messageContent.equals("!disconnect"))
+            {
+                disconnect();
+            }
+        });
     }
 
     private void disconnect()
@@ -33,6 +48,7 @@ public class Sadge {
     public static void main(String args[])
     {
         Sadge sadge = new Sadge("NzI0NzU0MTU1NjQxNTAzODA0.XviWmg._v0TY1r_Hs8xDGSGK90mtsE_QaU");
+        sadge.start();
     }
 
 }
